54c0f0ecf32526a8295e02243e0e8e0ee644c38b
Michael Fisher <mirfishe@gmail.com>
2020-09-25 16:16:38 -0400

    removed the code using the any data type

--- src/components/NytApp.tsx
+++ src/components/NytApp.tsx
@@ -6,7 +6,18 @@ type testState = {
     startDate: string,
     endDate: string,
     pageNumber: number,
-    results: any
+    // results: any,
+    // resultDataItem: {},
+    resultData: resultDataItem[]
+};
+
+type resultDataItem = {
+  index: string,
+  headlineMain: string,
+  multimediaURL: string,
+  snippet: string,
+  keywords: string[]
+  web_url: string
 };
 
 class NytApp extends Component<{}, testState> {
@@ -18,7 +29,14 @@ class NytApp extends Component<{}, testState> {
             startDate: "",
             endDate: "",
             pageNumber: 0,
-            results: []
+            // results: [],
+            // resultDataItem: {
+            //   index: "",
+            //   headlineMain: "",
+            //   snippet: "",
+            //   web_url: ""
+            // },
+            resultData: []
         };
 
         this.fetchResults = this.fetchResults.bind(this);
@@ -27,7 +45,7 @@ class NytApp extends Component<{}, testState> {
 
     };
 
-    fetchResults = () => {
+     fetchResults = () => {
 
         const baseURL:string = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
         const key:string | undefined | null = process.env.REACT_APP_NYT_API_KEY;
@@ -35,10 +53,97 @@ class NytApp extends Component<{}, testState> {
         let url:string = `${baseURL}?api-key=${key}&page=${this.state.pageNumber}&q=${this.state.search}`;
         url = this.state.startDate ? url + `&begin_date=${this.state.startDate}` : url;
         url = this.state.endDate ? url + `&end_date=${this.state.endDate}` : url;
-    
+  
+
         fetch(url)
           .then(res => res.json())
-          .then(data => this.setState({results: data.response.docs}))
+          .then(data => {
+            // this.setState({results: data.response.docs});
+
+            console.log('data', data);
+            // console.log('data.length', data.length);
+
+            // console.log('data.response.docs', data.response.docs);
+            // console.log('data.response.docs.length', data.response.docs.length);
+
+            // local array of objects to replace values in resultList
+            // let resultDataItems: {index: string,
+            //   headlineMain: string,
+            //   snippet: string,
+            //   web_url: string}[] = [];
+
+            // let resultDataItems: {resultDataItem: resultDataItem}[] = [];
+            let resultDataItems: resultDataItem[] = [];
+
+            let resultDataItem: resultDataItem;
+
+            let arrKeywords: string[] = [];
+            let multimediaLink: string = "";
+
+            for (let i = 0; i < data.response.docs.length; i++) {
+              // this.setState({resultDataItem: {
+              //   index: data.response.docs[i]._id,
+              //   headlineMain: data.response.docs[i].headline.main,
+              //   snippet: data.response.docs[i].snippet,
+              //   web_url: data.response.docs[i].web_url
+              // }})
+
+              arrKeywords = [];
+              // console.log('data.response.docs[i].keywords', data.response.docs[i].keywords);
+              // console.log('data.response.docs[i].keywords.length', data.response.docs[i].keywords.length);
+              for (let j = 0; j < data.response.docs[i].keywords.length; j++) {
+                arrKeywords.push(data.response.docs[i].keywords[j].value);
+              };
+              // console.log('arrKeywords', arrKeywords);
+
+              multimediaLink = "";
+              // console.log('data.response.docs[i].multimedia', data.response.docs[i].multimedia);
+              // console.log('data.response.docs[i].multimedia.length', data.response.docs[i].multimedia.length);
+              // for (let j = 0; j < data.response.docs[i].multimedia.length; j++) {
+              //   if (j === 0) {
+              //   multimediaLink = data.response.docs[i].multimedia[j].url;
+              //   break;
+              //   };
+              // };
+              if (data.response.docs[i].multimedia.length > 0) {
+                multimediaLink = data.response.docs[i].multimedia[0].url;
+              };
+              // console.log('multimediaLink', multimediaLink);
+
+              resultDataItem = {
+                  index: data.response.docs[i]._id,
+                  headlineMain: data.response.docs[i].headline.main,
+                  multimediaURL: multimediaLink,
+                  snippet: data.response.docs[i].snippet,
+                  keywords: arrKeywords,
+                  web_url: data.response.docs[i].web_url
+                };
+
+              resultDataItems.push(resultDataItem);
+
+              // console.log('this.state.resultDataItem', this.state.resultDataItem);
+
+              // push to an array
+              // resultDataItems.push({
+              //   index: data.response.docs[i]._id,
+              //   headlineMain: data.response.docs[i].headline.main,
+              //   snippet: data.response.docs[i].snippet,
+              //   web_url: data.response.docs[i].web_url
+              // });
+
+              // console.log('index', data.response.docs[i]._id);
+              // console.log('headlineMain', data.response.docs[i].headline.main);
+              // console.log('snippet', data.response.docs[i].snippet);
+              // console.log('web_url', data.response.docs[i].web_url);
+
+          };
+
+          // console.log('resultDataItems', resultDataItems);
+
+          // setState of that array with the local array
+          this.setState({resultData: resultDataItems});
+
+          })
           .catch(err => console.log(err));
         
     };
@@ -72,6 +177,12 @@ class NytApp extends Component<{}, testState> {
 
       };
 
+    componentDidUpdate() {
+
+        // this.fetchResults();
+
+    };
+
 
     render() {
 
@@ -90,7 +201,8 @@ class NytApp extends Component<{}, testState> {
                 <br />
                 <button className="submit">Submit search</button>
               </form>
-              {this.state.results.length > 0 ? <NytResults results={this.state.results} changePage={this.changePageNumber} /> : null}
+              {/* {this.state.results.length > 0 ? <NytResults results={this.state.results} changePage={this.changePageNumber} /> : null} */}
+              {this.state.resultData.length > 0 ? <NytResults resultData={this.state.resultData} changePage={this.changePageNumber} /> : null}
             </div>
           </div>
         );
--- src/components/NytResults.tsx
+++ src/components/NytResults.tsx
@@ -1,28 +1,42 @@
 import React, {FunctionComponent} from 'react';
 
 type AcceptedProps = {
-    results: any,
-    changePage: (event: {}, direction: string) => void;
+    // results: any,
+    changePage: (event: {}, direction: string) => void,
+    resultData: resultDataItem[]
+};
+
+type resultDataItem = {
+  index: string,
+  headlineMain: string,
+  multimediaURL: string,
+  snippet: string,
+  keywords: string[]
+  web_url: string
 };
 
 const NytResults: FunctionComponent <(AcceptedProps)> = props => {
 
-    console.log('props.results', props.results);
+    // console.log('props.results', props.results);
 
     return(
         <div>
-        {props.results.map((result: any) => {
+        {/* {props.results.map((result: any) => { */}
+        {props.resultData.map((result: resultDataItem) => {
           return (
+            // <div key={result._id}>
+            //   <h2>{result.headline.main}</h2>
+            //   {result.multimedia.length > 1 ? <img alt="article" src={`http://www.nytimes.com/${result.multimedia[1].url}`} /> : ''}
             <div key={result.index}>
-              <h2>{result.headline.main}</h2>
-              {result.multimedia.length > 1 ? <img alt="article" src={`http://www.nytimes.com/${result.multimedia[1].url}`} /> : ''}
+              <h2>{result.headlineMain}</h2>
+              <img alt="article" src={`http://www.nytimes.com/${result.multimediaURL}`} />
               <p>
                 {result.snippet}
                 <br />
                 {result.keywords.length > 0 ? ' Keywords: ' : ''}
               </p>
               <ul>
-                {result.keywords.map((keyword: any) => <li key={keyword.value}>{keyword.value}</li>)}
+                {result.keywords.map((keyword: string) => <li key={keyword}>{keyword}</li>)}
               </ul>
               <a href={result.web_url}><button>Read It</button></a>
             </div>
